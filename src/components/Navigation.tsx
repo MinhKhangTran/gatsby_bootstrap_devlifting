df553f1c8d64b6b0fbd9204427929c3b3838c1e3
MinhKhangTran <minhkhang.tran@gmx.de>
2021-11-27 09:08:13 +0100

    :sparkles: navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,27 +1,40 @@
 import { Link } from "gatsby";
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useMediaQuery } from "../hooks/useMediaQuery";
+import logo from "../images/logo.svg";
+import logo_big from "../images/logo_big.svg";
 
 const Navigation = () => {
+  let isPageWide = useMediaQuery("(min-width: 992px)");
   return (
-    <Navbar bg="white" expand="lg">
+    <Navbar bg="transparent" expand="lg">
       <Container>
-        <Link to="/" className="navbar-brand text-primary">
-          NavbarBrand
+        <Link
+          to="/"
+          className="navbar-brand text-primary d-flex align-items-center"
+        >
+          <img src={isPageWide ? logo_big : logo} alt="logo of devlifting" />
         </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" data-cy="nav_button" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          data-cy="nav_button"
+        ></Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav" data-cy="nav_collapse">
           <Nav className="me-auto align-lg-items-center">
             <Nav.Link href="#hero">Home</Nav.Link>
-            <Nav.Link href="#menu">Features</Nav.Link>
-            <Nav.Link href="#gallery">Project</Nav.Link>
+            <Nav.Link href="#benefits">Benefits</Nav.Link>
+            <Nav.Link href="#process">Process</Nav.Link>
             <Link className="nav-link" to="/about">
               About
             </Link>
+            <Link className="nav-link" to="/blog">
+              Blog
+            </Link>
           </Nav>
-          <a className="btn btn-outline-primary" href="#contact">
+          <Link className="btn btn-outline-primary" to="/contact">
             Contact
-          </a>
+          </Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
