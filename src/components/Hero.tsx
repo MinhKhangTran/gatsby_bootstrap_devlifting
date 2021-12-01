2aaeb26b9510053d4fc34eb16f8ed885e5d663fc
MinhKhangTran <67480568+MinhKhangTran@users.noreply.github.com>
2021-12-01 17:39:38 +0100

    :sparkles: fixing hero translation bug

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,8 +13,8 @@ const Hero = () => {
             <h1><Trans>We do all the heavy lifting to build your web presence up</Trans></h1>
             <p className="lead">
             <Trans>
-              Our focus is growing your business by making your websites
-              accessible and conversion optimized!</Trans>
+              Our focus is growing your business by making your websites accessible and conversion optimized!
+              </Trans>
             </p>
             <div className="hero_buttons d-flex gap-3 justify-content-center justify-content-lg-start mt-4 mt-lg-5">
               <Link to="/work" className="btn btn-primary btn-lg">
